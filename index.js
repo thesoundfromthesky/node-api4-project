fc9d2bbd313bd2954068c0124c66e909ef165d6c
thesoundfromthesky <nagara@naver.com>
2020-01-16 11:03:04 -0600

    env update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const server = require("./server");
 // code away!
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`Server on ${port}`);
